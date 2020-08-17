b192fbe4c8e5b4cc06922c21429b498bce8a29cc
Freezefaz <m.fareez.aziz@gmail.com>
2020-08-17 09:25:36 +0000

    created templates for officers and form, added route to create officer

--- app.py
+++ app.py
@@ -9,14 +9,43 @@ load_dotenv()
 app = Flask(__name__)
 
 MONGO_URI = os.environ.get('MONGO_URI')
-DB_NAME = "safetynet"
+DB_NAME = 'safetynet'
 client = pymongo.MongoClient(MONGO_URI)
 db = client[DB_NAME]
 
-@app.route("/officers")
-def show_reports():
+@app.route('/officers')
+def show_officers():
     all_officers = db.safety_officers.find()
-    return render_template("show_reports.template.html", officers=all_officers)
+    return render_template("show_officers.template.html", 
+                            officers=all_officers)
+
+@app.route('/officers/create')
+def create_officers():
+    return render_template("create_officers.template.html")
+
+@app.route('/officers/create', methods=["POST"])
+def process_create_officers():
+    # process the form
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+    contact_number = request.form.get("contact_number")
+    email = request.form.get("email")
+    return render_template("create_officers.template.html",
+                            first_name=first_name,
+                            last_name=last_name,
+                            contact_number=contact_number,
+                            email=email)
+    # create the query
+    new_officer = {
+        "first_name": name,
+        "last_name": last_name,
+        "contact_number": contact_number,
+        "email": email
+    }
+
+    # Add the query to the database and the front page
+    db.safety_officers.insert_one(new_officer)
+    return redirect(url_for("show_officers"))
 
 
 # "magic code" -- boilerplate
