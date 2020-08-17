4942c3835b549cec70e0d78b96490aa184a63628
Freezefaz <m.fareez.aziz@gmail.com>
2020-08-17 06:21:08 +0000

    installed pymongo, dnspython, dotenv and create mongo link

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
+import pymongo
+import dnspython
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+
+MONGO_URI = os.environ.get('MONGO_URI')
+DB_NAME = "safetynet"
+client = pymongo.MongoClient(MONGO_URI)
 
 # "magic code" -- boilerplate
 if __name__ == '__main__':
